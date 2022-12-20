dbe8f752863ddd2294589455b2e96c18ea0bbbaf
Dipendra Dangol <dipendra.dangol11@gmail.com>
2022-12-20 11:14:21 +0545

    initial commit1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <div >
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
