c3f0b365833a80c90cb90033758aa910dd08cb3f
Dipendra Dangol <dipendra.dangol11@gmail.com>
2022-12-20 21:16:07 +0545

    created routing setup and new note form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Container } from "react-bootstrap"
+import { NewNote } from "./NewNote"
+
+export type Note = {
+  id: string
+} & NoteData
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <div >
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </div>
+      <Container className="my-4">
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/new" element={<h1>{<NewNote />}</h1>} />
+          <Route path="/:id">
+            <Route index element={<h1>Show</h1>} />
+            <Route path="edit" element={<h1>Edit</h1>} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
   )
 }
 
